04804ae2da6d2c83f5bcc521ca2725be9af97f87
Antoine <antoine.oriol@protonmail.com>
2022-05-25 13:17:58 +0200

    Modif helper

--- Site/helper.php
+++ Site/helper.php
@@ -11,23 +11,29 @@ function executeRequest($url,$data,$post){
     // rajoute l'option POST à la requete
     curl_setopt($ch,CURLOPT_POST, $post);
 
-    // rajoute le contenu de la requete
-    curl_setopt($ch,CURLOPT_POSTFIELDS, $data);
+    // rajoute l'option POST à la requete
+    if (isset($post) && !empty($post)) {
+        curl_setopt($ch,CURLOPT_POST, $post);
+    }
 
-    // on sait pas
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+    // rajoute le contenu de la requete
+    if (isset($data) && !empty($data)) {
+        curl_setopt($ch,CURLOPT_POSTFIELDS, $data);
+    }
     
     $headers = array();
     if (isset($_SESSION["id"]) && !empty($_SESSION["id"])) {
         array_push($headers,"User-Id:".$_SESSION["id"]);
     }
-
     curl_setopt($ch,CURLOPT_HTTPHEADER, $headers);
+    
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
     $res = curl_exec($ch);
+
     if ($res==false) {
 	    return (object)array("Success"=>false, "Error"=>"Erreur lors de l'appel de l'api");
     }
-    
     // retourne sous forme d'objet php les valeurs retournées en json
     return json_decode($res);
 }
